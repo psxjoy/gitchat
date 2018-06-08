2295ed7f8c7326c2d4278de21383a096a4520b0e
Panshuxin <jiaxiaoying@jxy.local>
2018-06-09 07:31:29 +0800

    Initial commit

---
+++ chapter7/crawl_info.py
@@ -0,0 +1,10 @@
+'''
+封装网络获取的信息
+'''
+import requests
+from bs4 import BeautifulSoup
+
+def get_info(url):
+    response = requests.get(url)
+    if response.status_code ==200:
+        response.encoding =   
