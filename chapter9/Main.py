395cc3ddd134fd585969733df69ffccf79c3d0af
elegantking <psxjoy@outlook.com>
2018-07-01 19:35:18 +0800

    commit chapter9 files

--- chapter9/Main.py
+++ chapter9/Main.py
@@ -4,4 +4,4 @@ from chapter9.send_sms import send_sms
 if __name__ == '__main__':
     url = '	http://api.map.baidu.com/telematics/v3/weather?location=杭州&output=json&ak=你的AK值'
     text = get_info(url)
-    send_sms(text)
+    send_sms("要发送的手机号码","Twilio提供的号码",text)
--- chapter9/send_sms.py
+++ chapter9/send_sms.py
@@ -3,12 +3,12 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "AC141778d2eed2ba022112c91157426103"
 # Your Auth Token from twilio.com/console
-auth_token  = "55738589b6ac6246896de0977500e63e"
+auth_token  = ""
 
 client = Client(account_sid, auth_token)
-def send_sms(sms_text):
+def send_sms(to_number,from_number,sms_text):
     message = client.messages.create(
-        to="你的手机号码",
-        from_="twilio给你的号码",
+        to=to_number,
+        from_=from_number,
         body=sms_text)
     print(message.sid)
